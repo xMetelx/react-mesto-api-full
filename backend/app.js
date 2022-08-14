e907f106bdeb36b79475b1e7c781a1f688889dd8
xMetelx <xmetelx@yandex.ru>
2022-08-14 23:12:42 +0300

    options

--- backend/app.js
+++ backend/app.js
@@ -31,7 +31,7 @@ const options = {
     'http://api.metel.nomoredomains.sbs',
     'https://api.metel.nomoredomains.sbs',
   ],
-  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
   preflightContinue: false,
   optionsSuccessStatus: 204,
   allowedHeaders: ['Content-Type', 'origin', 'Authorization'],
