6dd01824bd505bae30608841ea997615006be1a8
xMetelx <xmetelx@yandex.ru>
2022-08-29 21:59:39 +0300

    Port

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -2,5 +2,5 @@ module.exports = {
   env: process.env.NODE_ENV || 'development',
   serverDb: process.env.SERVER_DB || 'mongodb://localhost:27017/mestodb',
   jwtSecret: process.env.JWT_SECRET || 'dev-secret',
-  POST: process.env.PORT,
+  PORT: process.env.PORT,
 };
